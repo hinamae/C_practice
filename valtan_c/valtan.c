8a36b8a5c2d532d3da6e69f8df42694c90b59bf4
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-06-27 20:49:01 +0900

    [modify]バルタン星人が動いた

--- valtan_c/valtan.c
+++ valtan_c/valtan.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include	<stdio.h>
-#include	<unistd.h> // sleep(),usleep()
-#include	"c_game.h"
+#include <stdio.h>
+#include <unistd.h> // sleep(),usleep()
 
+//  gcc  valtan.c -o  val
 //ターミナルの制御コード。エスケープシーエンスの開始
 // #define ESC \033
 #define WAIT (10000000/2)
 #define LOOP 200
 
 int main(void){
-    int xpos =8;
-    int ypos = 8;
+    int xpos =1;
+    int ypos = 1;
     int i = 0;
     long j;
 	printf("\033[2J") ;
 
 
-    for(i=0; i<200; i+=2){
+    for(i=0; i<2000; i+=2){
         //端まで行ったら一段下がる
         xpos = i%80+1;
         if (xpos==1) ypos++;
@@ -25,17 +25,17 @@ int main(void){
         printf("\033[%d;%dH  Yo¥¥oY" , ypos ,xpos);
         printf("\033[%d;%dH    w   " , ypos+1 ,xpos);
         printf("\033[%d;%dH   | |   " , ypos+2 ,xpos);
-
+        fflush(stdout);//バッファをフラッシュ
         for (j=0; j<WAIT ; j++){ }
 
          // 腕を下げる
         printf("\033[%d;%dH   o¥¥o " , ypos ,xpos+1);
         printf("\033[%d;%dH >- w -<" , ypos+1 ,xpos+1);
         printf("\033[%d;%dH   < >  " , ypos+2 ,xpos+1);
+        fflush(stdout);//バッファをフラッシュ
         for (j=0; j<WAIT ; j++){ }
-
         }
-    printf("/nPress [Enter]");
+    printf("\nPress [Enter]");
     (void)getchar();//エンターキーを押せば終了
 
     // printf("%c[>51" );//カーソルを表示
