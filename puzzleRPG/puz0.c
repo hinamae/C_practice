294dde9c431597e903de674d9fbe7229f4cf1914
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-07-05 04:21:21 +0900

    [modify ]構造体など定義

--- puzzleRPG/puz0.c
+++ puzzleRPG/puz0.c
@@ -6,19 +6,24 @@
 
 
 // 列挙型宣言
-
+typedef enum Element{FIRE, WALTER, WIND, EARTH, LIFE, EMPTY}Element;
 
 // グローバル定数の宣言
+const char ELEMENT_SYMBOLS[EMPTY+1]={'$','~','@','#','&',' '};
+const char ESCAPE_SEQ_COLOR[EMPTY+1]={1,6,2,3,5,0};
 
 
-// // 構造体型宣言
-// typedef struct MONSTER{
-//     char* name;
-// }Monster;
+
+
+typedef struct DUNGEON{
+    Monster* monsters;
+    int Monsters_num;
+} Dungeon;
 
 // プロトタイプ宣言
 void goDungeon(char* playerName);
-void DoBattle(char* monster_pointer);
+void DoBattle(Monster doraque_monster);
+
 
 
 // 関数宣言
@@ -26,22 +31,34 @@ void DoBattle(char* monster_pointer);
 int main(int argc, char** argv){
 
     printf("***puzzle & monsters***\n");
-    char* monster1 = "スライム";
-    char* monster2 = "メタルスライム";
-    char* monster3 = "ドラキー";
-    char* monster4 = "モーモン";
-    char* monster5 = "キングメタル";
 
-    char* monster_pointers[5] = {monster1,
-                               monster2,
-                               monster3,
-                               monster4,
-                               monster5
-                               };
+    int monster_num=5;
+    Monster doraque_Monsters[] = {
+        {"スライム",100,100,WALTER,10,5},
+        {"メタルスライム",200,200,EARTH,20,15},
+        {"ドラキー",300,300,WIND,30,25},
+        {"モーモン",400,400,WIND,40,30},
+        {"キングメタル",800,800,FIRE,50,40}
+    };
+    Dungeon dungeon = {doraque_Monsters , 5};
+    Dungeon* dungeon_p;
+
+    // char* monster1 = "スライム";
+    // char* monster2 = "メタルスライム";
+    // char* monster3 = "ドラキー";
+    // char* monster4 = "モーモン";
+    // char* monster5 = "キングメタル";
+
+    // char* monster_pointers[5] = {monster1,
+    //                            monster2,
+    //                            monster3,
+    //                            monster4,
+    //                            monster5
+    //                            };
     goDungeon(argv[1]);
     int i;
-    for(i=0 ; i<5 ; i++){
-    DoBattle(monster_pointers[i]);
+    for(i=0 ; i< dungeon_p->Monsters_num ; i++){
+        DoBattle(doraque_Monsters[i]);
     }
     return 0;
 
@@ -50,3 +67,4 @@ int main(int argc, char** argv){
 
 
 // ユーティリティ関数宣言
+
--- puzzleRPG/puz1.c
+++ puzzleRPG/puz1.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+// 構造体型宣言
+typedef struct MONSTER{
+    char* name;
+    int HP;
+    int maxHP;
+    int type;
+    int attack;
+    int defense;
+}Monster;
+
+// struct Monster;
 
 void goDungeon(char* playerName){
     printf("%sはダンジョンに到着した!!\n",playerName);
 }
 
-void DoBattle(char* monster_pointers){
-    printf( "%sが現れた\n",monster_pointers);
-    printf( "%sを倒した！\n",monster_pointers);
+void DoBattle(Monster doraque_monster){
+    printf( "%sが現れた\n",doraque_monster.name);
+    printf( "%sを倒した！\n",doraque_monster.name);
 
 }
+
+// void printMonsterName(){
+
+// }
